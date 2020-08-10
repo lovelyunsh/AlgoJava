2ec284f592ee0a38312e8573b0a54d5a52fbb70c
seunghwan <lovelyunsh@naver.com>
2020-08-10 20:55:05 +0900

    213

--- 백준/src/백준/P1697숨바꼭질.java
+++ 백준/src/백준/P1697숨바꼭질.java
@@ -1,66 +1,48 @@
 package 백준;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
-class find {
-	int now;
-	int cnt;
-	public find(int now, int cnt) {
-		super();
-		this.now = now;
-		this.cnt = cnt;
-	}
-	
-}
+
 public class P1697숨바꼭질 {
-	
-	public static void main(String[] args) throws IOException {
+	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		boolean check [] = new boolean[1000000];
 		StringTokenizer st;
 
 		st = new StringTokenizer(br.readLine());
 
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-		Queue<find> que = new LinkedList<find>();
-		que.add(new find(N, 0));
-		int result = 0;
-		while(!que.isEmpty()) {
-			find a = que.poll();
-			check[N] = true;
-			if(a.now == K) {
-				result = a.cnt;
-				break;
-			}
-			
-			if(a.now + 1 > K) {
-				que.offer(new find(a.now-1, a.cnt+1));
-			}
-			else if(a.now == 0) {
-				que.offer(new find(a.now+1, a.cnt+1));
-			}
-			else {
-				que.offer(new find(a.now*2, a.cnt+1));
-				que.offer(new find(a.now+1, a.cnt+1));
-				if(!check[a.now-1]) {
-					que.offer(new find(a.now-1, a.cnt+1));
-				}
-				if(a.now+1 < 1000000 &&!check[a.now+1]) {
-					que.offer(new find(a.now-1, a.cnt+1));
-				}
-				if(a.now*2 < 1000000 && !check[a.now*2]) {
-					que.offer(new find(a.now-1, a.cnt+1));
+		
+		int[] min = new int[100005];
+		System.out.println(bfs(N, K, min));
+
+	}
+
+	public static int bfs(int N, int K, int[] map) {
+		int next = N;
+		int[] now = new int[3];
+		Queue<Integer> que = new LinkedList<Integer>();
+		que.add(next);
+		map[next] = 0;
+
+		while (!que.isEmpty() && next != K) {
+			next = que.poll();
+			now[0] = next - 1;
+			now[1] = next + 1;
+			now[2] = next * 2;
+			for (int i = 0; i < 3; i++) {
+				if (now[i] >= 0 && now[i] <= 100000) {
+					if (map[now[i]] == 0) {
+						que.add(now[i]);
+						map[now[i]] = map[next] + 1;
+					}
 				}
 			}
 		}
-		
-		System.out.println(result);
-		
+		int result = map[K];
+		return result;
 	}
-
 }
---
+++ 수업/src/회귀/순열만들어보자.java
@@ -0,0 +1,27 @@
+package 회귀;
+
+import java.util.Scanner;
+
+public class 순열만들어보자 {
+	static int N;
+	static int P;
+	static int arr[];
+	
+	static void perm() {
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		P = sc.nextInt();
+		arr = new int[N];
+		for(int i = 0 ; i < N ; i++)
+			arr[i] = i+1;
+		
+	}
+
+}
