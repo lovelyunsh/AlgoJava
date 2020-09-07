46cf5ce2d0df143eb9ee861de2be221ebadbd62f
seunghwan <lovelyunsh@naver.com>
2020-09-07 21:50:35 +0900

    12

--- SwExperAcademy/src/D4/P2105디저트카페.java
+++ SwExperAcademy/src/D4/P2105디저트카페.java
@@ -5,7 +5,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class P2105디저트카페 {
+public class P2105디저트카페 {//1
 	static int map[][],N;
 	static int dr[] = {1,1,-1,-1}; 
 	static int dc[] = {1,-1,-1,1};
