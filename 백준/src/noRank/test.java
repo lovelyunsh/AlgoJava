92be468ee8ff635c27258fb2e03394b673e6493e
seunghwan <lovelyunsh@naver.com>
2020-09-17 15:12:13 +0900

    123

--- 백준/src/noRank/test.java
+++ 백준/src/noRank/test.java
@@ -1,11 +1,22 @@
 package noRank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class test {
 	public static void main(String[] args) {
-		String a = "aaaaa";
-		String b = a.substring(0,1);
 		
-		System.out.println(b);
+		List<String> a = new ArrayList<>();
+		a.add("123");
+		a.add("2123");
+		a.add("3123");
+		
+		
+		String temp = a.get(0);
+		a.set(0, a.get(2));
+		a.set(2, temp);
+		
+	
 		
 		
 	}
