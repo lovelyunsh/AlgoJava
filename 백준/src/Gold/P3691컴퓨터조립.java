be6bc0c9b00ec3843df81fe6dd898fa0ddfc7838
seunghwan <lovelyunsh@naver.com>
2020-10-29 23:12:58 +0900

    123

--- 백준/src/Gold/P3691컴퓨터조립.java
+++ 백준/src/Gold/P3691컴퓨터조립.java
@@ -2,12 +2,9 @@ package Gold;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class P3691컴퓨터조립 {
@@ -23,9 +20,23 @@ public class P3691컴퓨터조립 {
 
 		@Override
 		public int compareTo(product o) {
+			return Long.compare(this.price, o.price);
+		}
+	}
+	static class sProduct implements Comparable<sProduct>{
+		long price;
+		long quality;
+		String type;
+		public sProduct(long price, long quality, String type) {
+			super();
+			this.price = price;
+			this.quality = quality;
+			this.type = type;
+		}
+		@Override
+		public int compareTo(sProduct o) {
 			return Long.compare(this.quality, o.quality);
 		}
-
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -36,73 +47,58 @@ public class P3691컴퓨터조립 {
 			st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
 			int B = Integer.parseInt(st.nextToken());
-			HashMap<String, List<product>> map = new HashMap<String, List<product>>();
-			String firstKey = null;
+			Map<String, PriorityQueue<product>> map = new HashMap<String, PriorityQueue<product>>();
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				String type = st.nextToken();
-				if (i == 0)
-					firstKey = type;
 				st.nextToken();
-				int price = Integer.parseInt(st.nextToken());
-				int quality = Integer.parseInt(st.nextToken());
+				long price = Long.parseLong(st.nextToken());
+				long quality = Long.parseLong(st.nextToken());
 				if (map.get(type) == null) {
-					map.put(type, new ArrayList<product>());
+					map.put(type, new PriorityQueue<product>());
 				}
-				map.get(type).add(new product(price,quality));
+				map.get(type).offer(new product(price,quality));
 			}
-			Collections.sort(map.get(firstKey));
-			Map<Long, Long> dp = new LinkedHashMap<Long, Long>();
-			for (product p : map.get(firstKey)) {
-				dp.put(p.quality, p.price);
+			//젤 싼 부품들로 일단 만들고
+			//퀄리티 젤 낮은 얘들만 꺼내서 젤 싼 부품 꺼내서 퀄리티 올리기
+			//하나라도 더 이상 꺼낼게 없거나 가격이 안맞으면 끝 
+			long quality = Long.MAX_VALUE;
+			long price = 0;
+			PriorityQueue<sProduct> selected = new PriorityQueue<sProduct>();
+			for(String type : map.keySet()) {
+				product cheap = map.get(type).poll();
+				price += cheap.price;
+				quality = Math.min(quality, cheap.quality);
+				selected.offer(new sProduct(cheap.price, cheap.quality, type));
 			}
-			map.remove(firstKey);
-			for (String type : map.keySet()) {
-				Map<Long, Long> temp = new LinkedHashMap<Long, Long>();
-				List<product> products = map.get(type);
-				Collections.sort(products);
-				for (product p : products) {
-					long prePrice = Integer.MAX_VALUE;
-					long preQuality = Integer.MAX_VALUE;
-					boolean end = true;
-					for (long dd : dp.keySet()) { // 안되면 2진 탐색 ㄱ
-						
-						if (p.quality < dd) {
-							end = false;
-							if (prePrice + p.price <= B) {
-								if (temp.get(preQuality) != null && temp.get(preQuality) < prePrice + p.price) {
-								} else {
-									temp.put(preQuality, prePrice + p.price);
-								}
-							}
-							if (p.price + dp.get(dd) <= B) {
-								if (temp.get(p.quality) != null && temp.get(p.quality) < prePrice + p.price) {
-								} else {
-									temp.put(p.quality, prePrice + dp.get(dd));
-								}
-							}
-							break;
-						} // 마지막꺼 더해야대
-						preQuality = dd;
-						prePrice = dp.get(dd);
-					}
-					if(end) {
-						if (prePrice + p.price <= B) {
-							if (temp.get(preQuality) != null && temp.get(preQuality) < prePrice + p.price) {
-							} else {
-								temp.put(preQuality, prePrice + p.price);
-							}
-						}
-					}
+			long result = 0;
+			while(true) {
+				sProduct now = selected.poll();
+				boolean canChange = false;
+				product change = null;
+				while(true) { //능력 더 좋은 놈 뽑기
+					if(map.get(now.type).isEmpty())
+						break;
+					change = map.get(now.type).poll();
+					if(change.quality<= now.quality)
+						continue;
+					if(price - now.price + change.price > B)
+						break;
+					canChange = true;
+					break;
 				}
-				dp = temp;
+				if(canChange) {
+					price = price - now.price + change.price;
+					selected.offer(new sProduct(change.price, change.quality, now.type));
+				}
+				else {
+					result = now.quality;
+					break;
+				}
 			}
-			long max = 0;
-			for(long p : dp.keySet()) {
-				max = Math.max(max, p);
-			}
-			System.out.println(max);
-
+			System.out.println(result);
+			
+			
 		}
 
 	}
