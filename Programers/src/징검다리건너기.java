bf006b8fb7ad6f34c5b17350621d03b460a9eec2
seunghwan <lovelyunsh@naver.com>
2021-06-22 09:54:43 +0900

    프로그래머스 문제 추가

---
+++ Programers/src/징검다리건너기.java
@@ -0,0 +1,17 @@
+
+public class 징검다리건너기 {
+	
+	
+	public int solution(int[] stones, int k) {
+		int min = Integer.MAX_VALUE;
+		for(int i = 0 ; i < stones.length-k+1; i++) {
+			int max = -1;
+			for(int j = i ; j < i +k ; j++)
+				max = Math.max(stones[j], max);
+			min = Math.min(min, max);
+		}
+		
+		
+		return min;
+	}
+}
--- 백준/src/noRank/test.java
+++ 백준/src/noRank/test.java
@@ -1,32 +1,10 @@
 package noRank;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class test {
-	static int num = -1;
-	static class ab implements Comparable<ab>{
-		int a;
-		
-		@Override
-		public int compareTo(ab o) {
-			// TODO Auto-generated method stub
-			return num*this.a - num*o.a;
-		}
-
-		public ab(int a) {
-			this.a = a;
-		}
-		
-	}
-	
 	public static void main(String[] args) {
-		ArrayList<Integer> a = new ArrayList<Integer>();
-		a.add(1);
-		a.add(2);
-		List<Integer> b = a.subList(1, 2);
-		System.out.println(Arrays.toString(b.toArray()));
+		String a = "123";
+		String b = a;
+		a = "234";
+		System.out.println(b);
 	}
-
 }
