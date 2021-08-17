ef150245a92600248b545ef056686794d4deaa10
lovelyunsh <lovelyunsh@naver.com>
2021-08-17 20:29:03 +0900

    mac add pro

--- Programmers/src/test.java
+++ Programmers/src/test.java
@@ -1,22 +1,42 @@
+import java.awt.*;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 public class test {
+	static class testclass{
+		int x;
+		int y;
+
+		public testclass(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			testclass testclass = (testclass) o;
+			return x == testclass.x && y == testclass.y;
+		}
+	}
 
 	public static void main(String[] args) {
-		int h= 2;
-		int m = 12;
-		int s= 5;
-		System.out.println(String.format("%02d:%02d:%02d",h,m,s));
-		
-		HashSet<HashSet> a = new HashSet<>();
-		HashSet<Integer> l1 =new HashSet<Integer>(Arrays.asList(1,2,3));
-		HashSet<Integer> l2 =new HashSet<Integer>(Arrays.asList(1,2,3));
-		a.add(l1);
-		a.add(l2);
-		
-		
-		
-		System.out.println(1);
+		Set<List> a = new HashSet<>();
+		testclass tcA = new testclass(1,2);
+		testclass tcB = new testclass(1,2);
+		List<Integer> list1 = Arrays.asList(1,2,3);
+		List<Integer> list2 = Arrays.asList(1,2,3);
+
+		a.add(list1);
+		a.add(list2);
+
+		System.out.println(a);
+
 	}
 }
+
+
